937d78d9875fe1073f39bed2751ef24b38b7ea9d
Onur Gulsan <onurgulsan@gmail.com>
2023-04-19 16:08:53 -0400

    Created basic child class of discord.Client

--- TABot.py
+++ TABot.py
@@ -1,6 +1,22 @@
+import discord
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Hey! {self.user.name} here and ready to roll')
 
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        await message.channel.send("Hi!")
+
+
+intents = discord.Intents.default() # establish intents to pass into Client constructor
+intents.message_content = True
+
+load_dotenv(".env")  # take environment variables from .env.
 token = os.getenv("DISCORD_BOT_TOKEN")
+
+client = MyClient(intents=intents) # create an instance of MyClient and pass in wanted intents
+client.run(token)
