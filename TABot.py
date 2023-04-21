1069d074c491e2854607773a9a91295688050b9a
Onur Gulsan <onurgulsan@gmail.com>
2023-04-21 17:20:13 -0400

    Added greeting message when a new member joins and updated intents

--- TABot.py
+++ TABot.py
@@ -1,13 +1,16 @@
 import discord
 import os
-from HelperFunctions import getGeneralTextChannel
+from HelperFunctions import getGeneralTextChannelFromGuilds
 from dotenv import load_dotenv
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        general_chat = getGeneralTextChannel(self.guilds)
+        general_chat = getGeneralTextChannelFromGuilds(self.guilds)
         await general_chat.send("Greetings students!") # sends a greeting message into the channel
 
+    async def on_member_join(self, member):
+        general_chat = discord.utils.get(member.guild.text_channels, name = "general") # grabs the general text channel
+        await general_chat.send((f'Hello {member.name} and welcome to the {member.guild.name} server!'))
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -16,6 +19,7 @@ class MyClient(discord.Client):
 
 intents = discord.Intents.default() # establish intents to pass into Client constructor
 intents.message_content = True
+intents.members = True
 
 load_dotenv(".env")  # loads variables from .env
 token = os.getenv("DISCORD_BOT_TOKEN") # retrieve my BOT's token
