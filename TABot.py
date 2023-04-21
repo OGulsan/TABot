6946d6aac8b46f0a40932ec239e7dd06339d5b49
Onur Gulsan <onurgulsan@gmail.com>
2023-04-21 14:47:57 -0400

    Refactor greeting message implementation

--- TABot.py
+++ TABot.py
@@ -6,15 +6,14 @@ from dotenv import load_dotenv
 class MyClient(discord.Client):
     async def on_ready(self):
         for guild in self.guilds: # for each guild
-            for channel in guild.channels: # for each channel in each guild
-                if isinstance(channel, discord.TextChannel) and channel.name == "general": # display a greeting message to all text channels named 'general'
-                    await channel.send("Greetings students!")
+            general_chat = discord.utils.get(guild.channels, name = "general") # grabs the general text channel
+            await general_chat.send("Greetings students!") # sends a greeting message into the channel
 
     async def on_message(self, message):
         if message.author == self.user:
             return
         
-        
+
 intents = discord.Intents.default() # establish intents to pass into Client constructor
 intents.message_content = True
 
