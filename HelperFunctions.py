4c628ca339c51d2272010bfe400d02c7f639e221
Onur Gulsan <onurgulsan@gmail.com>
2023-04-21 17:31:28 -0400

    Added functionality to test if a message is a command

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -4,3 +4,8 @@ def getGeneralTextChannelFromGuilds(guilds):
     for guild in guilds: # for each guild
             general_chat = discord.utils.get(guild.text_channels, name = "general") # grabs the general text channel
     return general_chat
+
+def isCommand(message):
+      if(message.content.startswith('!')):
+            return True
+      return False
