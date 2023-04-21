26413d272a51bfee188021073d3c4c1686ee05da
Onur Gulsan <onurgulsan@gmail.com>
2023-04-21 17:19:46 -0400

    Refactored function declaration

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,6 +1,6 @@
 import discord
 
-def getGeneralTextChannel(guilds):
+def getGeneralTextChannelFromGuilds(guilds):
     for guild in guilds: # for each guild
             general_chat = discord.utils.get(guild.text_channels, name = "general") # grabs the general text channel
     return general_chat
