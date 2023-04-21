a3be75a3052891b5ef3a1d1a64898f6f8d5a7f41
Onur Gulsan <onurgulsan@gmail.com>
2023-04-21 15:55:46 -0400

    Searching through text channels only

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -2,5 +2,5 @@ import discord
 
 def getGeneralTextChannel(guilds):
     for guild in guilds: # for each guild
-            general_chat = discord.utils.get(guild.channels, name = "general") # grabs the general text channel
+            general_chat = discord.utils.get(guild.text_channels, name = "general") # grabs the general text channel
     return general_chat
